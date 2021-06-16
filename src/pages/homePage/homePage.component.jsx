b0eb39003874846812f50e797fc3a278f4d697e5
anisomegala <anielsomeillan@icloud.com>
2021-06-16 13:29:57 +0200

    final folder estructure

---
+++ src/pages/homePage/homePage.component.jsx
@@ -0,0 +1,12 @@
+import React from  'react';
+import { Directory } from '../../components/directory/directory.jsx'
+
+import './homePage.styles.scss';
+
+
+export const HomePage = () => (
+    <div className="homepage">
+        <Directory />
+    </div>
+
+)
