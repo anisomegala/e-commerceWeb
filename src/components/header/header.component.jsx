cce62c8e4a4c412efaf44eb7ac186c6166c069e8
anisomegala <anielsomeillan@icloud.com>
2021-06-22 07:48:59 +0200

    added header component and close icon component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../assets/aniel.svg'
 import './header.style.scss';
 
 
 const Header = () => (
     <div className='header'>
-        <Link className='logo' to='/'>
-             AniShop
+        <Link className='logo-container' to='/'>
+             <Logo className='logo' />
         </Link>
         <div className='options-links'> 
             <Link className='option' to='/shop'>
